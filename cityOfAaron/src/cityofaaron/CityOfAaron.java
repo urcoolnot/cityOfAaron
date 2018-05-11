ecdf64dd2caae5948c6bbc8e361840ca4464a25c
jarednlee <jarednlee@gmail.com>
2018-05-10 21:28:33 -0700

    5-10-2018 Created Project 5-10-2018 Added Comments

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+* The main() class file for the cityOfAaron project 
  */
 package cityofaaron;
 
